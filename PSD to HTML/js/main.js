9c26de4dfa5627feea2669020c83989252c5abed
rockinrobin714 <robindykema@gmail.com>
2017-02-12 16:22:13 -0600

    FINISHED

--- PSD to HTML/js/main.js
+++ PSD to HTML/js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
     new WOW().init();
     
+    $('.burger-nav').on('click', function(){
+        $('.container nav ul').toggleClass('open');
+    })
+    
     $('.js-wp-2').waypoint(function (direction) {
         $('.js-wp-2').addClass('animated slideInUp');
     }, {
