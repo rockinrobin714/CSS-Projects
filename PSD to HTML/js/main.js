91d39600a18e77d48e2ff017e7eaaa108a1b7098
rockinrobin714 <robindykema@gmail.com>
2017-02-12 14:56:20 -0600

    BUGZZZ errywhere

--- PSD to HTML/js/main.js
+++ PSD to HTML/js/main.js
@@ -1,2 +1,13 @@
-
-
+$(document).ready (function() {
+    
+   $('.js-wp-2').waypoint(function(direction) {
+       
+       $('.js-wp-2').addClass('animated slideInUp');
+       
+   }, {
+		offset: '70%'
+	});
+    
+    $('iphone-btn').delay(2300).animate( {bottom: '+=-3'}, 300)
+    
+});
