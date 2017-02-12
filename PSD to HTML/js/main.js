51b7f1cfccaf4cd0cef3cd1d50c635923a6b138d
rockinrobin714 <robindykema@gmail.com>
2017-02-12 15:07:09 -0600

    Phone is animated

--- PSD to HTML/js/main.js
+++ PSD to HTML/js/main.js
@@ -1,13 +1,21 @@
-$(document).ready (function() {
+$(document).ready(function () {
+    $('.js-wp-2').waypoint(function (direction) {
+        $('.js-wp-2').addClass('animated slideInUp');
+    }, {
+        offset: '70%'
+    });
     
-   $('.js-wp-2').waypoint(function(direction) {
-       
-       $('.js-wp-2').addClass('animated slideInUp');
-       
-   }, {
-		offset: '70%'
-	});
+    $('.js-wp-3').waypoint(function (direction) {
+        $('.js-wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '70%'
+    });
     
-    $('iphone-btn').delay(2300).animate( {bottom: '+=-3'}, 300)
+    $(".iphone-btn").delay(2300).animate({
+        bottom: "+=-3"
+    }, 300);
     
+    $(".iphone-btn").delay(300).animate({
+        top: "+=-3"
+    }, 100);
 });
